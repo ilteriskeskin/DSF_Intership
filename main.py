7c5a0ebfd3f15c7cfd1bfc4e2ebc85b10e067a89
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-06-27 00:37:40 +0300

    Başlangıç yapılandırmaları yapıldı

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas
+import csv
+
+# with open('slcsp/slcsp.csv') as slcsp_reader:
+#     slcsp_reader = csv.reader(slcsp_reader)
+#     line_count = 0
+#     for row in slcsp_reader:
+#         if line_count == 1:
+#             zip_codes.append(row)
+#         else:
+#             line_count += 1
+
+# ihtiyacım olan şey plan_id'deki (row[0]) son 5 eleman ve karşılığı olan rate değeri
+
+# with open('deneme.csv') as deneme_file:
+#     deneme_reader = csv.reader(deneme_file, delimiter=',')
+#     line_count = 0
+#     for row in deneme_reader:
+#         if line_count == 1:
+#             plans.append(row[0])
+#         else:
+#             line_count += 1
+
+plan = pandas.read_csv('deneme.csv')
+zips = pandas.read_csv('deneme2.csv')
+
+for i in range(len(plan)):
+    temp = plan.plan_id[i]
+    for j in range(len(zips)):
+        if int(temp[-5:]) == int(zips.zipcode[j]):
+            zips.rate.to_csv(str(plan.rate[i]))
